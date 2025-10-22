8e51786474954c3d8b9b118a94c1b47e23e4a0ef
kevin kalenga <kevinkalenga10@gmail.com>
2025-10-22 10:57:06 +0200

    Header and Footer components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import {Inter} from "next/font/google";
 import "@/assets/styles/globals.css";
-import { APP_NAME } from "@/lib/constants";
+import Header from "@/components/shared/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({subsets: ['latin']})
 
 export const metadata: Metadata = {
-  title: `${APP_NAME}`,
+ 
+  title: 'Home',
   description: "A modern ecommerce platform build with Next.js",
 };
 
@@ -17,10 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex h-screen flex-col">
-        
+        <Header />
         <main className="flex-1 wrapper">
             {children}
         </main>
+        <Footer />
     </div>
   );
 }
