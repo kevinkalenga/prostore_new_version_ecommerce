feb0d54be74edf0235d4a5cb01cf85ba79565a23
kevin kalenga <kevinkalenga10@gmail.com>
2025-10-27 11:26:43 +0100

    Seed sample data

--- db/seed.ts
+++ db/seed.ts
@@ -1,18 +1,32 @@
-import { PrismaClient } from '../lib/generated/prisma';
-import sampleData from './sample-data' 
+// import { PrismaClient } from '../lib/generated/prisma';
+// import { PrismaClient } from '../lib/generated/prisma/index.js';
+// import sampleData from './sample-data' 
 
+
+// async function main() {
+//     const prisma = new PrismaClient()
+//     await prisma.product.deleteMany()
+
+//     await prisma.product.createMany({data: sampleData.products})
+
+//     console.log("db seeded successfully!")
+// }
+// main()
+
+
+import { PrismaClient } from '@prisma/client';
+import sampleData from './sample-data';
+
+const prisma = new PrismaClient();
 
 async function main() {
-    const prisma = new PrismaClient()
-    await prisma.product.deleteMany()
-    await prisma.account.deleteMany()
-    await prisma.session.deleteMany()
-    await prisma.verificationToken.deleteMany()
-    await prisma.user.deleteMany()
-
-    await prisma.product.createMany({data: sampleData.products})
-    await prisma.user.createMany({data: sampleData.users})
-
-    console.log("db seeded successfully!")
+  await prisma.product.deleteMany();
+  await prisma.product.createMany({ data: sampleData.products });
+  console.log("db seeded successfully!");
 }
+
 main()
+  .catch(console.error)
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
